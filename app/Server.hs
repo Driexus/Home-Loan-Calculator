9af6a21f5724ec3670c9e209b02ec08dcca5327e
Driexus <toliasdimitris@gmail.com>
2025-03-21 00:17:47 +0200

    Add server

---
+++ app/Server.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Main where
+
+import Web.Scotty
+import GHC.Generics
+import Data.Aeson (ToJSON)
+import Modules.LoanData
+import Modules.Projections
+
+main = scotty 3000 $ do
+    get "/:word" $ do
+        beam <- pathParam "word"
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+    get "/projection/:projectionName" $ do
+        name <- pathParam "projectionName"
+        let maybeProjection = projectionFromName name
+        case maybeProjection of
+            Just projection -> json $ Response (projection lData)
+            Nothing -> return()
+
+data Response = Response {
+    projection :: [Float]
+} deriving (Show, Generic)
+
+instance ToJSON Response
+
+-- TODO: Transfer to LoanData as base
+lData :: LoanData
+lData = LoanData 15000 4 9 5 9 0.5 500 4 10 300 1000 150000 600 300
+
+    --"initialInvestment" : 15000,
+    --"interest" : 4,
+    --"yearlyRoI": 9,
+    --"inflation" : 5,
+    --"yearlySalaryIncrease" : 9,
+    --"propertyAppreciation" : 4.5,
+    --"currentRent" : 500,
+    --"rentIncrease" : 4,
+    --"durationYears" : 10,
+    --"maintenanceFees" : 300,
+    --"salary" : 1000,
+    --"propertyValue" : 150000,
+    --"baseCoL" : 600,
+    --"basePropertyCosts": 300
+--
+--
+--
+    --initialInvestment :: Float,
+    --interest :: Float,
+    --yearlyRoI :: Float,
+    --inflation :: Float,
+    --yearlySalaryIncrease :: Float,
+    --propertyAppreciation :: Float,
+    --currentRent :: Float,
+    --rentIncrease :: Float,
+    ---- Loan Duration in Years
+    --durationYears :: Int,
+    --maintenanceFees :: Float,
+    --salary :: Float,
+    --propertyValue :: Float,
+    --baseCoL :: Float,
+    --basePropertyCosts :: Float
