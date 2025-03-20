cc62eccec43a33d5c18de510e018de9e3a7e7b8a
Driexus <toliasdimitris@gmail.com>
2025-03-20 23:37:49 +0200

    Add qhd, fhd options for cell sizing. Fixes #2

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,10 @@ import Modules.Utils
 import Text.JSON.Generic
 import Options.Applicative
 
-project :: Projection -> IO()
-project func = do 
+project :: Projection -> Int -> IO()
+project func size = do 
     loanData <- loadData
-    printList (func loanData) 12
+    printList (func loanData) 12 size
 
 loadData :: IO LoanData
 loadData = do 
@@ -24,21 +24,23 @@ main = run =<< execParser opts
      <> header "A simple program to calculate future monetary values")
 
 run :: Options -> IO ()
-run (Options name p) = do
+run (Options name pretty raw) = do
   let maybeProjection = projectionFromName name
   case maybeProjection of
-    Just projection -> printProjection projection p
+    Just projection -> printProjection projection raw pretty
     Nothing -> print $ "Option '" ++ name ++ "' could not be found. Run -h for help."
 
-printProjection :: Projection -> Bool -> IO()
-printProjection proj True = project proj
-printProjection proj False = do
+printProjection :: Projection -> Bool -> String -> IO()
+printProjection proj False "qhd" = project proj 21
+printProjection proj False _ = project proj 15
+printProjection proj True _ = do
   l <- loadData
   mapM_ print (proj l)
 
 data Options = Options {
   target     :: String,
-  pretty     :: Bool
+  pretty     :: String,
+  raw        :: Bool
 }
 
 options :: Parser Options
@@ -47,8 +49,15 @@ options = Options
         metavar "Target" <>
         help "The target function to run"
       )
-      <*> switch (
+      <*> strOption (
         long "pretty" <>
         short 'p' <>
-        help "Enable pretty printing"
+        metavar "fhd or qhd" <>
+        value "fhd" <>
+        help "Enable pretty printing. Pass 'fhd' for Full HD screens and 'qhd' for for Quad HD."
+      )
+      <*> switch (
+        long "raw" <>
+        short 'r' <>
+        help "Print the values in seperate lines. This overrides pretty printing."
       )
--- app/modules/Utils.hs
+++ app/modules/Utils.hs
@@ -1,51 +1,44 @@
 module Modules.Utils where
 
-printList :: Show a => [a] -> Int -> IO ()
-printList [] _ = return ()
-printList list elementsPerLine = do
-    putStrLn $ showElementsRecursive (orderedPrefix "Month " elementsPerLine) elementSize (replicate firstRowSpace ' ' ++ "|")
-    printListRecursive splitted 1 elementSize
+printList :: Show a => [a] -> Int -> Int -> IO ()
+printList [] _ _= return ()
+printList list elementsPerLine cellSize = do
+    putStrLn $ showElementsRecursive (orderedPrefix "Month " elementsPerLine) size (replicate firstRowSpace ' ' ++ "|")
+    printListRecursive splitted 1 size
     where splitted = chunksOf list elementsPerLine
           elementSize = maximum $ map (length . show) list
+          size = max (elementSize + 2) cellSize
 
 printListRecursive :: Show a => [[a]] -> Int -> Int -> IO ()
 printListRecursive [] _ _ = return ()
-printListRecursive (x:xs) current elementSize = do
-    putStrLn $ verticalLine elementSize (length x)
-    putStrLn (showConstant firstRowSpace ("Year " ++  show current) ++ showElementsRecursive (map show x) elementSize "|")
-    printListRecursive xs (current + 1) elementSize
+printListRecursive (x:xs) current cellSize = do
+    putStrLn $ verticalLine cellSize (length x)
+    putStrLn (showConstant firstRowSpace ("Year " ++  show current) ++ showElementsRecursive (map show x) cellSize "|")
+    printListRecursive xs (current + 1) cellSize
 
 showElementsRecursive :: [String] -> Int -> String -> String
 showElementsRecursive [] _ current = current
-showElementsRecursive (x:xs) maxSize current = current ++ showElementsRecursive xs maxSize (showConstantCentered (maxSize + elementSpacing) x ++ "|")
+showElementsRecursive (x:xs) cellSize current = current ++ showElementsRecursive xs cellSize (showConstantCentered cellSize x ++ "|")
 
+-- Fills empty spaces at the end to match the given size. If the size is less then does nothing.
 showConstant :: Int -> String -> String
 showConstant size a = a ++ extraChars
     where extraChars = concat $ replicate (size - length a) " "
 
+-- Fills empty spaces at the end to match the given size. If the size is less then does nothing.
 showConstantCentered :: Int -> String -> String
 showConstantCentered size a = replicate (extraChars `div` 2) ' ' ++ a ++ replicate ((extraChars + 1) `div` 2) ' '
     where extraChars = size - length a
 
 verticalLine :: Int -> Int -> String
-verticalLine size elements = replicate (firstRowSpace + 1 + (cellSize size) * elements) '-'
+verticalLine size elements = replicate (firstRowSpace + (size + 1) * elements) '-'
 
 orderedPrefix :: String -> Int -> [String]
 orderedPrefix prefix total = map ((++) prefix . show) [1..total]
 
 -- TODO: Put in config file
 firstRowSpace :: Int
-firstRowSpace = 4 + elementSpacing
-
--- TODO: Put in config file
-elementSpacing :: Int
-elementSpacing = 4
-
-emptyElementSpacing :: String
-emptyElementSpacing = replicate elementSpacing ' '
-
-cellSize :: Int -> Int
-cellSize elementSize = elementSize + elementSpacing + 1
+firstRowSpace = 10
 
 chunksOf :: [a] -> Int -> [[a]]
 chunksOf list elements = chunksOfRecursive list elements []
